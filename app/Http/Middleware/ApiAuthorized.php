1342cb54de0548089e30895bcd675c0fc21ee0c4
Genming <genmingzhao@gmail.com>
2019-01-23 08:31:42 -0500

    add middleware for api_key

---
+++ app/Http/Middleware/ApiAuthorized.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ApiAuthorized
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $api_key = $request->input('api_key');//default:12345678
+        if ($api_key !== env('API_KEY')) {
+            return response()->json(['error' => "API key is not correct"], 500);
+        }
+        return $next($request);
+    }
+}
